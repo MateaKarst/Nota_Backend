ce51ed10e64412d27d5fa672a4ce9b67afbd371c
MateaKarst <matea.karst@gmail.com>
2025-03-06 07:44:36 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+// Entry point for the backend
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
+
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
